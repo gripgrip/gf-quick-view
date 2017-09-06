0e9feaaee205d9fca303b9f59cb32738f9aaf44a
gripgrip <mirceas17@gmail.com>
2017-09-06 21:36:00 +0300

    Add license in plugin file

--- class-gf-quick-view.php
+++ class-gf-quick-view.php
@@ -7,6 +7,20 @@ Author: Mircian
 Version: 1.0.0
 Author URI: https://mircian.com
 Text Domain: gfqv
+License: GPL2
+
+Gf-quick-view is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+any later version.
+
+Gf-quick-view is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Gf-quick-view. If not, see http://www.gnu.org/licenses/gpl.html.
 */
 
 class GF_Quick_View {
